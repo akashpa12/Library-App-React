268d188e49c227658219fe67c7388aeb528fdda5
akashpa12 <akash.pa.anandan00@gmail.com>
2024-06-08 06:40:32 +0530

    messege

--- src/components/Navebar.jsx
+++ src/components/Navebar.jsx
@@ -4,30 +4,30 @@ import { Link } from 'react-router-dom'
 const Navebar = () => {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg bg-warning">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Library</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="/">Add Book</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/search">Search Book</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/delete">Delete Book</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/view All">View All</Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <nav class="navbar navbar-expand-lg bg-warning">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Library</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" to="/">Add Book</Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/search">Search Book</Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/delete">Delete Book</Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to="/view All">View All</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,54 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navebar from './Navebar'
 
 const ViewAll = () => {
+  const [data, changedata] = useState(
+    [
+      { "#": 2, "Book Title": "adn", "Author": "scn", "Published Year": 1234, "Price": 120 },
+      { "#": 2, "Book Title": "adn", "Author": "scn", "Published Year": 1234, "Price": 120 },
+
+    ]
+  )
   return (
     <div>
-        <Navebar/>
-        <div className="container">
-            <div className="row">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <div className="row g-3">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Book Title</th>
-      <th scope="col">Author</th>
-      <th scope="col">Published Year</th>
-      <th scope="col">Price</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Sherlock Holmes</td>
-      <td>Arthur Conan Doyale</td>
-      <td>2004</td>
-      <td>150</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Aadujeevitham</td>
-      <td>Benyamin</td>
-      <td>2017</td>
-      <td>150</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry the Bird</td>
-      <td>@twitter</td>
-      <td>2011</td>
-      <td>130</td>
-    </tr>
-  </tbody>
-</table>
-                    </div>
-                </div>
-                </div>
+      <Navebar />
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table">
+                <thead>
+        <tr>
+          <th scope="col">No</th>
+          <th scope="col">Book Title</th>
+          <th scope="col">Author</th>
+          <th scope="col">Published Year</th>
+          <th scope="col">Price</th>
+        </tr>
+      </thead>
+                  {data.map(
+                    (value, index) => {
+                      return <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Sherlock Holmes</td>
+          <td>Arthur Conan Doyale</td>
+          <td>2004</td>
+          <td>150</td>
+        </tr>
+      </tbody>
+
+                    }
+                  )
+                  }
+                </table>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
